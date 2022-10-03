9124b4d3a6aca28a58565839481af0d7e20eacb0
MacMini <>
2022-10-03 09:25:28 +0600

    Project created, Logo added

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by itiw-mac 256 on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
