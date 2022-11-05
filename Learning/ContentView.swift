e01694e05eb1318db61efc3028b7235788d4764c
MacMini <>
2022-11-06 00:21:45 +0600

    dynamic amount of cards shown.

--- Learning/ContentView.swift
+++ Learning/ContentView.swift
@@ -4,14 +4,28 @@
 //
 //  Created by itiw-mac 256 on 10/3/22.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    var emojies = ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🎾", "🏐", "🏉", "🥏", "🎱", "🪀", "🏓", "🏸", "🏒", "🏑", "🥍", "🏏", "🪃", "🥅", "⛳️", "🪁", "🛝", "🏹", "🎣", "🤿", "🥊", "🥋", "🎽", "🛹", "🛼", "🛷", "⛸", "🥌", "🎿", "⛷", "🏂"]
+    @State var emojiCount = 3
     var body: some View {
-        HStack{
-            CardView(text: "⚽️", isFaceUp: true)
-            CardView(text: "🏀")
+        VStack{
+            HStack{
+                ForEach(emojies[0..<emojiCount], id: \.self, content: { em in
+                    CardView(text: em)
+                })
+            }.foregroundColor(.green)
+            Spacer()
+            HStack{
+                Button {
+                    emojiCount = min(emojiCount+1, emojies.count)
+                } label: {Image(systemName:"plus.circle").imageScale(.large)}
+                Spacer()
+                Button{
+                    emojiCount = max(emojiCount-1, 1)
+                } label: {Image(systemName:"minus.circle").imageScale(.large)}
+            }.padding(.horizontal).font(.largeTitle)
         }.padding(.horizontal)
     }
 }
@@ -23,7 +37,7 @@ struct CardView: View{
         ZStack{
             let shape = RoundedRectangle(cornerRadius: 10)
             if(isFaceUp){
-                shape.fill(.orange)
+                shape
                 shape.stroke(lineWidth:5).foregroundColor(.green)
                 Text(text)
                     .font(.title)
