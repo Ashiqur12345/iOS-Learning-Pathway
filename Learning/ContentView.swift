0d18f87c803f0a1a69908573ab9ab15d9fe85fb5
MacMini <>
2022-11-05 17:39:08 +0600

    SwiftUI card game card view flip on tap gesture

--- Learning/ContentView.swift
+++ Learning/ContentView.swift
@@ -9,18 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack{
+            CardView(text: "⚽️", isFaceUp: true)
+            CardView(text: "🏀")
+        }.padding(.horizontal)
+    }
+}
+
+struct CardView: View{
+    let text : String
+    @State var isFaceUp = false
+    var body: some View{
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 10)
+            if(isFaceUp){
+                shape.fill(.orange)
+                shape.stroke(lineWidth:5).foregroundColor(.green)
+                Text(text)
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+            }
+            else{
+                shape.fill(.orange)
+            }
+        }.onTapGesture{
+            isFaceUp = !isFaceUp
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().preferredColorScheme(.light)
+//        ContentView().preferredColorScheme(.dark)
     }
 }
